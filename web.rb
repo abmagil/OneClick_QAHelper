64bc2cef716e83a8c711189dc0a846da5cd1758d
Aaron Magil <amagil@camsys.com>
2013-07-31 16:53:38 -0400

    Fixing logic to catch dev-test label

--- web.rb
+++ web.rb
@@ -24,7 +24,8 @@ post '/' do
         #Adding a comment will spawn another activity note.  Catch and move on if this is the case.
         next if xml_doc.at_xpath("//author").text.eql? "QA Helper" 
         #If the story is labeled with dev-test, don't update it on status transitions
-        next if full_story['story']['labels'].include 'dev-test'
+        next if full_story['story'].has_key?('labels') and full_story['story']['labels'].include? "dev-test"
+        puts full_story['story']['labels']
         updater = StoryUpdater.new(full_story)
         updater.update
       end
