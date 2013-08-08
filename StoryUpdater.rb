edbd9f2b6c6c3c8c24c6c2c78d57227c01cf4011
Aaron Magil <amagil@camsys.com>
2013-08-08 09:50:19 -0400

    Initial Refactor.  Needs debug

--- StoryUpdater.rb
+++ StoryUpdater.rb
@@ -8,41 +8,61 @@ class StoryUpdater
                              'Content-type' => 'application/xml'})
   end
   
-  def update
+  #Any triggers that need to fire based on ticket creation
+  def update_on_create
+    case @full_story['story_type']
+    when "chore"
+      set_labels(:add_dev_test)
+    end
+  end
+  
+  #Any triggers that need to fire based on ticket updates
+  def update_on_update
     case @full_story['story']['current_state']
         when "accepted"
-          update_story(:add_pending)
+          set_labels(:add_pending)
         when "rejected"
-          update_story(:remove_qa)
+          set_labels(:remove_qa)
         end
-        
+  end
+  
+  def set_labels(func)
+    labels = get_labels
+    self.send(func)
+    update_story({'labels'=>labels})
+  end
+  
+  def add_dev_test
+    labels.prepend("dev-test")
   end
   
   def add_pending
-    labels = get_labels
     labels.prepend("qa-pending,")
-    labels
   end
 
   def remove_qa
-    labels = get_labels
     labels = labels.gsub(/,?qa-pending,?/,',').gsub(/,?qa,?/,',')
-    labels
   end
   
   def get_labels
     @full_story['story']['labels'] || ""
   end
   
-  def update_story(func)
+  #Generic function to ingest updates and push them to PT
+  def update_story(h)
     target_url = BASEURL.gsub('PROJECT_ID',@full_story['story']['project_id'].to_s).gsub('STORY_ID',@full_story['story']['id'].to_s)
     update_xml = Nokogiri::XML::Builder.new do
       story {
-        labels my_strip(self.send(func),',')
+        to_nodes(h)
+        test "test"
       }
     end
     StoryUpdater.put(target_url,:body => update_xml.doc.root.to_xml)
   end
+end
+
+def to_nodes(h)
+  h.each {|key, value| puts "#{key} #{mystrip(value,',')}"}
 end
 
 def my_strip(string, chars)
--- web.rb
+++ web.rb
@@ -11,8 +11,22 @@ end
 
 post '/' do
   xml_doc = Nokogiri::XML(request.body)
-  #At present, no need to handle any transition that isn't a story update
   case xml_doc.at_xpath("//event_type").text
+    #Chore stories are not going to encapsulate QA-able tasks.  Label as dev-test
+    when "story_create"
+      project = xml_doc.at_xpath("//project_id").text
+      stories = xml_doc.root.xpath("//stories")
+      story_ids = stories.xpath(".//id")
+      #Labels do not come through on all activities, so we must access raw story data
+      story_ids.each do |story|
+        grabber = FullStoryGrabber.new(project,story)
+        grabber.get_story
+        full_story = grabber.full_story
+        next if full_story['story_type'] != 'chore' #TODO Clean up this logic
+        updater = StoryUpdater.new(full_story)
+        updater.update_on_create
+      end
+    #Transitions into Accepted automatically tag as "qa-pending"
     when "story_update" 
       project = xml_doc.at_xpath("//project_id").text
       stories = xml_doc.root.xpath("//stories")
@@ -27,7 +41,7 @@ post '/' do
         #If the story is labeled with dev-test, don't update it on status transitions
         next if full_story['story'].has_key?('labels') and full_story['story']['labels'].include? "dev-test"
         updater = StoryUpdater.new(full_story)
-        updater.update
+        updater.update_on_update
       end
     end
 end
